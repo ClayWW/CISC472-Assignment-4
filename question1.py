beea5aa486960a939f7620a8602aa44984d0ec67
Clay <clayw@udel.edu>
2023-10-01 23:40:10 -0400

    Added comments to 1a

--- question1.py
+++ question1.py
@@ -2,14 +2,20 @@ from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 from Crypto.Util import Counter
 
+#xor operation that takes in two strings of bytes
 def xor(a, b):
     return bytes(x ^ y for x,y in zip(a,b))
 
+#initialize AES cipher using a 128 bit key and 128 bit nonce
 def Init(key, nonce):
     ctr = Counter.new(128, initial_value=int.from_bytes(nonce, byteorder= "big"))
     cipher = AES.new(key, AES.MODE_CTR, counter=ctr)
     return cipher, nonce
 
+#updates the cipher (which is being used as the keystream in this situation)
+#takes in the previous state of the cipher and updates it by incrementing the nonce
+#for each block of plaintext. Also returns the updated nonce so that there are no
+#repeated keystreams
 def Update(previous):
     zero_key = b'\00'*16
     updated_nonce = (int.from_bytes(previous, byteorder="big")+1).to_bytes(16, byteorder="big")
@@ -17,6 +23,11 @@ def Update(previous):
     cipher = AES.new(zero_key, AES.MODE_CTR, counter=ctr)
     return cipher, updated_nonce
 
+#encrypts a plaintext block using a stateful cipher
+#takes in the plaintext, a 128 bit key, and a 128 bit nonce
+#breaks the plaintext into blocks and then uniquely encodes each block
+#while updating the nonce used for each block so that two blocks are not
+#encrypted using the same keystream
 def stateful_encrypt(plaintext, key, nonce):
     cipher, nonce = Init(key, nonce)
     ciphertext_blocks = []
