665e8785f41a3c1e8315c25ba67c0baf5cdae9c7
Clay <clayw@udel.edu>
2023-10-05 17:16:53 -0400

    Finished 1b

--- question1.py
+++ question1.py
@@ -7,19 +7,15 @@ from Crypto.Util.Padding import pad, unpad
 def xor(a, b):
     return bytes(x ^ y for x,y in zip(a,b))
 
-#initialize AES cipher using a 128 bit key and 128 bit nonce
-def Init(key, nonce):
-    cipher = AES.new(key, AES.MODE_ECB) #create the cipher and initialize it with the key and the nonce
-    return cipher.encrypt(nonce) #return the newly initialized cipher
 
-#updates the cipher (which is being used as the keystream in this situation)
-#takes in the previous state of the cipher and updates it by incrementing the nonce
-#for each block of plaintext. Also returns the updated nonce so that there are no
-#repeated keystreams
+def Init(key, nonce):
+    cipher = AES.new(key, AES.MODE_ECB) 
+    return cipher.encrypt(nonce) 
+
 def Update(previous):
-    zero_key = b'\00'*16 #use a zero key so that the cipher is initialized with the previous keystream (previous state)
-    new_cipher = AES.new(zero_key, AES.MODE_ECB) #pass the previous keystream into the cipher to create a new (updated) keystream
-    return new_cipher.encrypt(previous) #return the fresh cipher
+    zero_key = b'\00'*16 
+    new_cipher = AES.new(zero_key, AES.MODE_ECB) 
+    return new_cipher.encrypt(previous) 
 
 def stateful_sc(plaintext, key, nonce):
     keystream = Init(key, nonce)
@@ -32,47 +28,20 @@ def stateful_sc(plaintext, key, nonce):
 
     return b''.join(ciphertext_blocks)
 
-#encrypts a plaintext block using a stateful cipher
-#takes in the plaintext, a 128 bit key, and a 128 bit nonce
-#breaks the plaintext into blocks and then uniquely encodes each block
-#while updating the nonce used for each block so that two blocks are not
-#encrypted using the same keystream
-'''
-def stateful_sc(plaintext, key, nonce):
-    keystream = Init(key, nonce) #create the cipher
-    ciphertext_blocks = [] #create the array that holds the encrypted blocks
-    for i in range(0, len(plaintext), 16):
-        block = plaintext[i:i+16] #create the block by taking the next 16 bytes of plaintext
-        is_last_block = (i + 16) >= len(plaintext) #check to see if this iteration is handling the final block or not
-        if(len(block) < 16): #case for the final block if it is not a full block
-            full_keystream = cipher.encrypt(bytes([0]*16)) #create a full keystream that is 128 bits
-            keystream = full_keystream[:len(block)] #truncate the keystream to match the size of the block
-            encrypted_block = xor(block, keystream) #xor the truncated keystream with the block
-            ciphertext_blocks.append(encrypted_block) #add the encrypted block to the array
-            #do not need to update the cipher because being in this first if means that we are done after this
-        else: #case for either a full block or a final block that is full
-            keystream = cipher.encrypt(bytes([0]*16))
-            encrypted_block = xor(block, keystream) #xor the keystream with the block
-            ciphertext_blocks.append(encrypted_block) #add the encrypted block to the array
-            if not is_last_block: #no need to update the keystream if we're on the final block
-                cipher = Update(keystream) #pass the keystream into the update function to generate a fresh keystream for the next block
-
-    return b''.join(ciphertext_blocks) #join all the ciphertext blocks together and return
-'''
-def secure_PRP(key, input_block):
-    cipher = AES.new(key, AES.MODE_CBC, iv=0x00)
+def SC(input_block):
+    zero_key = b'\00'*16 
+    cipher = AES.new(zero_key, AES.MODE_ECB)
     return cipher.encrypt(input_block)
 
 
-def ctr_sc(plaintext, key, nonce):
+def ctr_sc(plaintext, nonce):
     ciphertext_blocks = []
     for i in range(0, len(plaintext), 16):
-
         block = plaintext[i:i+16]
         counter = i // 16
-        input_block = nonce + counter
-        zero_key = b'\x00'*16
-        keystream = secure_PRP(zero_key,input_block)
+        counter_bytes = counter.to_bytes(16, byteorder="big")
+        input_block = xor(nonce, pad(counter_bytes, 16))
+        keystream = SC(input_block)
         encrypted_block = xor(block, keystream)
         ciphertext_blocks.append(encrypted_block)
 
@@ -92,29 +61,9 @@ print(ciphertext_hex)
 
 print("\n")
 
-ciphertext_2 = stateful_sc(textforless_bytes, key, nonce)
-ciphertext_2hex = ciphertext_2.hex()
-print(ciphertext_2hex)
-
-print(f"Length of question1bytes in bytes: {len(question1bytes)}") 
-print(f"Length of question1bytes in bits: {len(question1bytes) * 8}")
-
-print(f"Length of ciphertext in bytes: {len(ciphertext)}")  
-print(f"Length of ciphertext in bits: {len(ciphertext) * 8}") 
-
-print(f"Length of ciphertext_hex in characters: {len(ciphertext_hex)}")  
-print(f"Length of ciphertext_hex in bits: {len(ciphertext_hex) * 4}")  
-
-
-
-print(f"Length of question1bytes in bytes: {len(textforless_bytes)}") 
-print(f"Length of question1bytes in bits: {len(textforless_bytes) * 8}")
-
-print(f"Length of ciphertext in bytes: {len(ciphertext_2)}")  
-print(f"Length of ciphertext in bits: {len(ciphertext_2) * 8}") 
-
-print(f"Length of ciphertext_hex in characters: {len(ciphertext_2hex)}")  
-print(f"Length of ciphertext_hex in bits: {len(ciphertext_2hex) * 4}")  
-
-
 question1btext = "Stream ciphers generate pseudorandom bits from a key and a nonce and encrypt the plaintext by XORing it with these pseudorandom bits, similar to the one time pad."
+nonce2 = bytes.fromhex("4a438deb82ced2faf3bd19fb4546c0ba")
+question2bytes = question1btext.encode('iso-8859-1')
+ciphertext2 = ctr_sc(question2bytes, nonce2)
+ciphertext2_hex = ciphertext2.hex()
+print(ciphertext2_hex)
